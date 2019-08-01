2490736eb5f32eec432f421aea6d98e70f20ae0c
monikanidhisingh <monikanidhisingh@gmail.com>
2019-08-01 13:45:05 +0530

    policy

---
+++ src/main/java/com/hcl/insurance/entity/Policy.java
@@ -0,0 +1,46 @@
+package com.hcl.insurance.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "policy")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "policyId")
+public class Policy {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="policy_id")
+	private Long policyId;
+	
+	@Column(name="policy_name")
+	private String policyName;
+	
+	@Column(name="policy_period")
+	private LocalDate policyPeriod;
+	
+	@Column(name="policy_from_age")
+	private Integer policyFromAge;
+	
+	@Column(name="policy_to_age")
+	private Integer policyToAge;
+	
+	@Column(name="policy_base_amount")
+	private Double policyBaseAmount;
+	
+	
+}
